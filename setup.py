33152faa72f122e41bc66f327f78acec71327431
JUANG <camiloang94@gmail.com>
2019-11-12 14:43:10 +0100

    Arrangements for new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 setup(
     name='stocker',
     packages=['stocker'],
-    version='0.1.2',
+    version='0.1.3',
     description='Stock Price Prediction',
     long_description=long_description,
     long_description_content_type="text/markdown",
