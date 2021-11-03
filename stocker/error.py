aaa8725ddd925179be76940091f574b4fb0f746c
J. Camilo G. Angarita <camiloang94@gmail.com>
2021-11-03 11:39:28 +0100

    Create error.py
    Good changes!, I've added a couple of tweaks for parentheses issue and take action when error_method != 'mape' or 'mse'

--- stocker/error.py
+++ stocker/error.py
@@ -2,14 +2,15 @@ from math import sqrt
 from sklearn.metrics import mean_squared_error
 
 
-def get(true_values, predicted_values, error_method='mape'): # function to calculate the error
-    error = 0
+def get(true_values, predicted_values, error_method='mape'):    # function to calculate the error
 
     if error_method == 'mape':
         # calculate the mean absolute percentage error
-        error = round(abs((true_values - predicted_values) / true_values).sum() / len(true_values)) * 100), 3)
+        return round((abs((true_values - predicted_values) / true_values).sum() / len(true_values)) * 100, 3)
+
     elif error_method == 'mse':
         # calculate the mean squared error
-        error = round(sqrt(mean_squared_error(true_values, predicted_values)), 3)
-
-    return error
+        return round(sqrt(mean_squared_error(true_values, predicted_values)), 3)
+        
+    else:
+        raise ValueError('This error method is not supported')
